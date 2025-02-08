57f3d4ff285277f7581c4bb27d2360012c9ebfba
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-02-08 15:14:36 +0330

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,9 +1,19 @@
+import subprocess
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext
+
+# تابع برای بررسی نسخه‌ی python-telegram-bot
+def get_pip_version():
+    result = subprocess.run(["pip", "show", "python-telegram-bot"], stdout=subprocess.PIPE)
+    return result.stdout.decode()
 
 # دستور start
 async def start(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text("سلام! من ربات بیمارستان میلاد هستم.")
+    
+    # بررسی نسخه‌ی کتابخانه python-telegram-bot و ارسال آن به کاربر
+    pip_version = get_pip_version()
+    await update.message.reply_text(f"نسخه python-telegram-bot: \n{pip_version}")
 
 # تابع اصلی برای راه‌اندازی ربات
 async def main():
